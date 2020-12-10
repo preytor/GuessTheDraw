4b25ae3b3781b61acdbe3ad618596e3a38012747
DAW <DAW@DESKTOP-R6429LO>
2020-12-10 10:27:43 +0100

    working class with constructor

--- GuessTheDraw/pintulogic.js
+++ GuessTheDraw/pintulogic.js
@@ -1,52 +1,59 @@
+class PintuLogic{
 
-var words_en = ["home", "beach"];
-var words_es = ["casa", "playa"];
+    constructor(id, lang) {
+        this.id = id;
+        this.lang = lang;
 
-function PintuLogic (id, lang) {
-    this.id = id;
-    this.lang = lang;
-}
-
-PintuLogic.setId = function(id){
-    this.id = id;
-};
-
-PintuLogic.getId = function() {
-    return this.id;
-};
-
-PintuLogic.setLang = function(lang){
-    this.lang = lang;
-};
-
-PintuLogic.getLang = function () {
-    return this.lang;
-};
-
-PintuLogic.setSecretWord = function (secretWord) {
-    this.secretWord = secretWord;
-};
-
-PintuLogic.getSecretWord = function () {
-    return this.secretWord;
-};
-
-PintuLogic.beginGame = function () {
-    PintuLogic.setSecretWord(selectSecretWord(this.lang));
-    console.log("secret word is " + secretWord);
-}
-
-function selectSecretWord() {
-    var secretWord;
-    switch (lang) {
-        case 'en':
-            secretWord = words_en[Math.floor((Math.random() * words_en.length))];
-            break;
-        case 'es':
-            secretWord = words_es[Math.floor((Math.random() * words_es.length))];
-            break;
-        default:
-            secretWord = words_en[Math.floor((Math.random() * words_en.length))];
+        console.log("pintulogic created");
     }
-    return secretWord;
+
+    setId = function (id) {
+        this.id = id;
+    };
+
+    getId = function () {
+        return this.id;
+    };
+
+    setLang = function (lang) {
+        this.lang = lang;
+    };
+
+    getLang = function () {
+        return this.lang;
+    };
+
+    setSecretWord = function (secretWord) {
+        this.secretWord = secretWord;
+    };
+
+    getSecretWord = function () {
+        return this.secretWord;
+    };
+
+    beginGame = function () {
+        setSecretWord(selectSecretWord(this.lang));
+        console.log("secret word is " + secretWord);
+    }
+
+    selectSecretWord = function () {
+        const words_en = ["home", "beach"];
+        const words_es = ["casa", "playa"];
+
+        var secretWord;
+        switch (lang) {
+            case 'en':
+                secretWord = words_en[Math.floor((Math.random() * words_en.length))];
+                break;
+            case 'es':
+                secretWord = words_es[Math.floor((Math.random() * words_es.length))];
+                break;
+            default:
+                secretWord = words_en[Math.floor((Math.random() * words_en.length))];
+        }
+        return secretWord;
+    }
+
 }
+
+module.exports = PintuLogic;
